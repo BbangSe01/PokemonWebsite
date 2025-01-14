57bb8eef1119f842b9e14f306c4d87c70c28eee1
BbangSe01 <155813689+BbangSe01@users.noreply.github.com>
2025-01-14 22:06:25 +0900

    feat: 메인페이지 UI 수정

--- PokemonWebsite/src/components/SelectGeneration.jsx
+++ PokemonWebsite/src/components/SelectGeneration.jsx
@@ -6,31 +6,44 @@ import g2Image from "../assets/Images/G2Image.webp";
 import g3Image from "../assets/Images/G3Image.webp";
 import g4Image from "../assets/Images/G4Image.webp";
 import Generation1 from "../components/Generations/Generation1";
+import MonsterballIcon from "../assets/Images/MonsterballIcon.png";
 const SelectGeneration = () => {
   //next
   // 관동지방부터 클릭 시 페이지 이동 및 151번까지 데이터 받아오고 처리하기
   return (
     <Map>
       <EachMap>
-        <MapTitle>관동지방</MapTitle>
+        <MapName>
+          <Monsterball src={MonsterballIcon}></Monsterball>
+          <MapTitle>관동지방 (1세대)</MapTitle>
+        </MapName>
         <Link to="/G1">
           <MapImage src={g1Image}></MapImage>
         </Link>
       </EachMap>
       <EachMap>
-        <MapTitle>성도지방</MapTitle>
+        <MapName>
+          <Monsterball src={MonsterballIcon}></Monsterball>
+          <MapTitle>성도지방 (2세대)</MapTitle>
+        </MapName>
         <Link to="/G2">
           <MapImage src={g2Image}></MapImage>
         </Link>
       </EachMap>
       <EachMap>
-        <MapTitle>호연지방</MapTitle>
+        <MapName>
+          <Monsterball src={MonsterballIcon}></Monsterball>
+          <MapTitle>호연지방 (3세대)</MapTitle>
+        </MapName>
         <Link to="/G3">
           <MapImage src={g3Image}></MapImage>
         </Link>
       </EachMap>
       <EachMap>
-        <MapTitle>신오지방</MapTitle>
+        <MapName>
+          <Monsterball src={MonsterballIcon}></Monsterball>
+          <MapTitle>신오지방 (4세대)</MapTitle>
+        </MapName>
         <Link to="/G4">
           <MapImage src={g4Image}></MapImage>
         </Link>
@@ -43,17 +56,16 @@ export default SelectGeneration;
 
 const Map = styled.div`
   width: 100%;
-  //   background-color: black;
   display: flex;
   flex-flow: wrap;
 `;
 
 const EachMap = styled.div`
-  width: 35%;
+  width: 30%;
   //   background-color: black;
   display: flex;
   flex-direction: column;
-  margin-left: 10%;
+  margin-left: 13%;
   margin-top: 2%;
   margin-bottom: 2%;
   //   border: 1px solid black;
@@ -63,4 +75,19 @@ const MapTitle = styled.h3``;
 
 const MapImage = styled.img`
   width: 100%;
+  height: 100%;
+  border: 3px solid black;
+  border-radius: 20px;
+`;
+
+const Monsterball = styled.img`
+  padding-top: 25px;
+  margin-right: 7px;
+  width: 20px;
+  height: 20px;
+`;
+
+const MapName = styled.div`
+  display: flex;
+  padding-left: 20px;
 `;
