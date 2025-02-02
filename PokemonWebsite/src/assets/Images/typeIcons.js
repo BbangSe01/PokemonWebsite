7598fe4e18383d2c833e91641d38bd34a7160a0c
BbangSe01 <155813689+BbangSe01@users.noreply.github.com>
2025-02-02 17:02:06 +0900

    [Feat] 포켓몬 타입별 이미지 및 css 생성

---
+++ PokemonWebsite/src/assets/Images/typeIcons.js
@@ -0,0 +1,41 @@
+import bug from "../Images/types/bug.svg";
+import dark from "../Images/types/dark.svg";
+import dragon from "../Images/types/dragon.svg";
+import electric from "../Images/types/electric.svg";
+import fairy from "../Images/types/fairy.svg";
+import fighting from "../Images/types/fighting.svg";
+import fire from "../Images/types/fire.svg";
+import flying from "../Images/types/flying.svg";
+import ghost from "../Images/types/ghost.svg";
+import grass from "../Images/types/grass.svg";
+import ground from "../Images/types/ground.svg";
+import ice from "../Images/types/ice.svg";
+import normal from "../Images/types/normal.svg";
+import poison from "../Images/types/poison.svg";
+import psychic from "../Images/types/psychic.svg";
+import rock from "../Images/types/rock.svg";
+import steel from "../Images/types/steel.svg";
+import water from "../Images/types/water.svg";
+
+const typeIcons = {
+  bug,
+  dark,
+  dragon,
+  electric,
+  fairy,
+  fighting,
+  fire,
+  flying,
+  ghost,
+  grass,
+  ground,
+  ice,
+  normal,
+  poison,
+  psychic,
+  rock,
+  steel,
+  water,
+};
+
+export default typeIcons;
