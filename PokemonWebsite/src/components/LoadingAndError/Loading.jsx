9c8063e6cb392af61d951d38517101dd66ce8ae6
BbangSe01 <155813689+BbangSe01@users.noreply.github.com>
2025-02-27 14:46:19 +0900

    [Feat] 도감 로딩페이지 생성

---
+++ PokemonWebsite/src/components/LoadingAndError/Loading.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import styled, { keyframes } from "styled-components";
+import Ev from "../../assets/Images/free-icon-game-14079448.png";
+
+const bounceInTop = keyframes`
+  0% {
+    transform: translateY(-250px);
+    animation-timing-function: ease-in;
+    opacity: 0;
+  }
+  38% {
+    transform: translateY(0);
+    animation-timing-function: ease-out;
+    opacity: 1;
+  }
+  55% {
+    transform: translateY(-65px);
+    animation-timing-function: ease-in;
+  }
+  72% {
+    transform: translateY(0);
+    animation-timing-function: ease-out;
+  }
+  81% {
+    transform: translateY(-28px);
+    animation-timing-function: ease-in;
+  }
+  90% {
+    transform: translateY(0);
+    animation-timing-function: ease-out;
+  }
+  95% {
+    transform: translateY(-8px);
+    animation-timing-function: ease-in;
+  }
+  100% {
+    transform: translateY(0);
+    animation-timing-function: ease-out;
+  }
+`;
+
+const Loading = () => {
+  return (
+    <Screen>
+      <LoadingMessage>로딩중입니다...</LoadingMessage>
+      <EvImg src={Ev} />
+    </Screen>
+  );
+};
+
+export default Loading;
+
+const Screen = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const LoadingMessage = styled.p`
+  font-family: "maple";
+  font-size: 60px;
+`;
+
+const EvImg = styled.img`
+  width: 100px;
+  height: 100px;
+  animation: ${bounceInTop} 1s both;
+  animation-iteration-count: infinite;
+`;
