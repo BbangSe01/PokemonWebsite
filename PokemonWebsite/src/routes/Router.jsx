cd577169a2fb44ae82ce52970ba7b3b1c48bf9d9
BbangSe01 <155813689+BbangSe01@users.noreply.github.com>
2025-02-27 16:58:16 +0900

    [Chore] 컨텐츠 한개 삭제

--- PokemonWebsite/src/routes/Router.jsx
+++ PokemonWebsite/src/routes/Router.jsx
@@ -6,7 +6,6 @@ import Generation3 from "../components/Generations/Generation3";
 import Generation4 from "../components/Generations/Generation4";
 import NotFound from "../components/Notfound";
 import Temp1 from "../components/Temporary1";
-import Temp2 from "../components/Temporary2";
 import Header from "../components/Header";
 import DetailPage from "../components/DetailPage/DetailPage";
 const Router = createBrowserRouter([
@@ -25,7 +24,6 @@ const Router = createBrowserRouter([
       { path: "/G3", element: <Generation3 /> },
       { path: "/G4", element: <Generation4 /> },
       { path: "/Temp1", element: <Temp1 /> },
-      { path: "/Temp2", element: <Temp2 /> },
       { path: "*", element: <NotFound /> },
       { path: "/DetailPage", element: <DetailPage /> },
     ],
