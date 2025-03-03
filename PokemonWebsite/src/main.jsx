3c443fe6f0fa08ea8f688227037f9fbb8fe790e0
BbangSe01 <155813689+BbangSe01@users.noreply.github.com>
2025-03-03 17:04:47 +0900

    [Feat] App 컴포넌트 Provider로 감쌈

--- PokemonWebsite/src/main.jsx
+++ PokemonWebsite/src/main.jsx
@@ -3,13 +3,16 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App.jsx";
-
+import store from "./stores/store.js";
+import { Provider } from "react-redux";
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </Provider>
   </StrictMode>
 );
